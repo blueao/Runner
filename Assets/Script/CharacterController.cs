c54f0ee4bc52ae0015cb413ce02015ae14b71cb5
Hieu Luu <hieuluuseafood@gmail.com>
2016-11-22 18:15:35 +0700

    Commit Scene

---
+++ Assets/Script/CharacterController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using DG.Tweening;
+public class CharacterController : MonoBehaviour
+{
+
+    public Rigidbody2D rigidbody;
+
+    float maxJumpHeight = 3.0f;
+    float groundHeight;
+    public Vector3 groundPos;
+    public readonly float jumpSpeed = 100f;
+    public readonly float fallSpeed = 1f;
+    public bool inputJump = false;
+    public bool grounded = false;
+    void Start()
+    {
+        groundPos = new Vector3(transform.position.x, transform.position.y+100,transform.position.z);
+        groundHeight = transform.position.y;
+        maxJumpHeight = transform.position.y + maxJumpHeight;
+    }
+    public void jumb()
+    {
+       // groundPos = new Vector3(transform.position.x, transform.position.y + 200, transform.position.z);
+        inputJump = true;
+      
+        //rigidbody.AddForce(Vector3.up * 500f, ForceMode2D.Impulse);
+    }
+    IEnumerator  Jump()
+    {
+        while (true)
+        {
+            if (transform.position.y >= maxJumpHeight)
+                inputJump = false;
+            if (inputJump)
+                transform.Translate(Vector3.up * jumpSpeed * Time.fixedDeltaTime);
+            else if (!inputJump)
+            {
+                transform.position = Vector3.Lerp(transform.position, groundPos, fallSpeed * Time.fixedDeltaTime);
+                if (transform.position.y >= groundPos.y-10f)
+                {
+                    transform.GetComponent<Rigidbody2D>().gravityScale =50;
+                    transform.GetComponent<Rigidbody2D>().isKinematic = false;
+                    StopAllCoroutines();
+                }
+            }
+
+           yield return new WaitForEndOfFrame();
+
+        }
+    }
+}
---
+++ Assets/Script/MainGameController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using System;
+public class MainGameController : MonoBehaviour
+{
+
+    public List<Transform> ListMap = new List<Transform>();
+    public CharacterController ModelController;
+    Vector3 EndVectorPosition;
+    float endposition;
+    float speed = 500f;
+    void Start()
+    {
+        endposition = ListMap[1].GetComponent<RectTransform>().rect.width;
+        EndVectorPosition = new Vector3(-endposition, ListMap[1].localPosition.y, 0);
+    }
+    void Update()
+    {
+        if (ModelController.grounded && Input.GetMouseButtonDown(0))
+        {
+            JumbModel();
+        }
+        Moveobject();
+        if (ModelController.transform.localPosition.y - (ListMap[0].localPosition.y + ListMap[0].GetComponent<RectTransform>().rect.height) < 1)
+        {
+
+            ModelController.grounded = true;
+        }
+        else
+        {
+            ModelController.grounded = false;
+        }
+    }
+    public void JumbModel()
+    {
+        Debug.Log((ListMap[0].localPosition.y + ListMap[0].GetComponent<RectTransform>().rect.height));
+        Debug.Log("jumb");
+        //ModelController.jumb();
+        ModelController.transform.GetComponent<Rigidbody2D>().gravityScale = 50;
+        ModelController.transform.GetComponent<Rigidbody2D>().isKinematic = false;
+        ModelController.transform.DOJump(new Vector3(ModelController.transform.position.x,ModelController.transform.position.y,ModelController.transform.position.z),100f,1,2).OnComplete(()=> {
+            ModelController.GetComponent<Rigidbody2D>().gravityScale = 0;
+            ModelController.GetComponent<Rigidbody2D>().isKinematic = true;
+        });
+    }
+    void Moveobject()
+    {
+        for (int i = 0; i < ListMap.Count; i++)
+        {
+            ListMap[i].localPosition = Vector3.MoveTowards(ListMap[i].localPosition, EndVectorPosition, Time.fixedDeltaTime * speed);
+            if (ListMap[i].localPosition == EndVectorPosition)
+            {
+                if (i + ListMap.Count - 1 < ListMap.Count)
+                {
+                    ListMap[i].localPosition = new Vector3(ListMap[i + 2].localPosition.x + ListMap[i].GetComponent<RectTransform>().rect.width - Time.fixedDeltaTime * speed, ListMap[i].localPosition.y, 0);
+                    Debug.Log(ListMap[i].localPosition.x + " ListMap[0].localPosition.x " + ListMap[i].localPosition.x);
+                    // UnityEditor.EditorApplication.isPaused = true;
+                }
+                else
+                    ListMap[i].localPosition = new Vector3(ListMap[i - 1].localPosition.x + ListMap[i].GetComponent<RectTransform>().rect.width, ListMap[i].localPosition.y, 0);
+            }
+        }
+    }
+}
